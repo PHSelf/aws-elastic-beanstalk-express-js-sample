6bfc080c258078ed54d788d078c5fb0a2ed5c6fc
שלום עייאש <pkubhcipkubh@gmail.com>
2021-08-24 12:23:35 +0300

    Add Joni

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hi PHSelf!!!'));
-app.get('/', (req, res) => res.send('Hi Jonathan!!!'));
+app.get('/', (req, res) => res.send('Hi PHSelf!!!, Hi Joni'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
